5839b1b044069450e5affea9da825386e0f580ed
Vũ Ngọc Quang Minh <minhngaongo1003@gmail.com>
2026-06-09 13:43:25 +0700

    chore(mobile): update lockfile and platform generated registrant files

--- apps/mobile/relax_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/mobile/relax_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,21 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
+import device_info_plus
 import file_selector_macos
 import flutter_secure_storage_macos
+import geolocator_apple
 import google_sign_in_ios
 import just_audio
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
